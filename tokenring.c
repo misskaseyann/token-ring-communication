8aa54a67b19713c50b1f145e52158274020dc2c3
Kasey Ann <kaseystowell@Kaseys-MacBook-Pro.local>
2018-09-25 11:17:35 -0500

    stop working on this

--- tokenring.c
+++ tokenring.c
@@ -5,17 +5,23 @@
 #include <sys/types.h>
 
 int main() {
-	pid_t pid, child;
+	pid_t pid, ppid; // do i need ppid?
 	int status;
 
-	if ((pid = fork()) < 0) {
-		perror("fork failure");
-		exit(1);
-	} else if (pid == 0) {
-		printf("Child %ld\n", (long) getpid());
-	} else {
-		sleep(1);
-		printf("Parent!")
-	}
+	char* data[1024];
+
+	printf("Enter a message: ");
+	scanf("%s", data);
+	printf("\nYou gave me: %s", data);
+
+	// if ((pid = fork()) < 0) {
+	// 	perror("fork failure");
+	// 	exit(1);
+	// } else if (pid == 0) {
+	// 	printf("Child %ld\n", (long) getpid());
+	// } else {
+	// 	sleep(1);
+	// 	printf("Parent!");
+	// }
 	return 0;
 }
