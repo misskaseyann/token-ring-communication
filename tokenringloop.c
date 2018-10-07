44a2f8868cb2869d2ef9eeece7d9a15a5d00218c
Kasey Ann <kaseystowell@Kaseys-MBP.attlocal.net>
2018-10-06 21:52:40 -0400

    add ending destination, comments, and small fixes

--- tokenringloop.c
+++ tokenringloop.c
@@ -9,29 +9,53 @@
 #define WRITE 1
 #define MAX 50
 
+/* Pipe array for file descriptors. */
 typedef int Pipe[2];
+/* Custom signal handler function. */
+void sigHandler(int sig, siginfo_t* si, void* context);
 
+/*
+ * TOKEN-RING COMMUNICATION SIM
+ * Using processes and pipes.
+ * 
+ * @author Kasey Stowell
+ * @version Fall 2018
+ */
 int main(int argc, char** argv) {
+	// Signal handling.
+	struct sigaction sVal;
+	sVal.sa_flags = SA_SIGINFO;
+	sVal.sa_sigaction = sigHandler;
+
 	// If the argument doesnt exist or is beyond limits, then set to 1.
 	int k = (argc > 1) && (atoi(argv[1]) > 0 && atoi(argv[1]) <= MAX) ? atoi(argv[1]) : 1;
+	// If second argument doesn't exist or is beyond limits, then set to parent.
+	int dest = (argc > 2) && (atoi(argv[2]) >= 0 && atoi(argv[2]) <= k) ? atoi(argv[2]) : 0;
+	// Bool for whether or not we actually reached our destination.
+	int dest_reached = 0;
+	// Parents access to pipe 0 and pipe k.
 	int c1_wr, ck_rd;
+	// Store parent ID.
 	int ppid = getpid();
 	printf("Parent with PID %d.\n", ppid);
-	sleep(3); // debug for tree
+	sleep(2); // Makes things more elegant.
 
 	// Create pipes array.
-	Pipe pipes[k];
+	Pipe pipes[k + 1];
 
 	// Initialize parent pipe.
 	if (pipe(pipes[0]) < 0) {
 		perror("Plumbing problem.\n");
 		exit(1);
 	}
-	printf("Made Pipe 0\n");
 
 	printf("Spawning %d processes.\n", k);
-	for (int i = 1; i < k; i++) {
-		// Make new pipe.
+	printf("Destination set to process %d.\n", dest);
+	printf("Made Pipe 0\n");
+
+	// Make remaining pipes.
+	for (int i = 1; i < k + 1; i++) {
+		// New pipe.
 		if (pipe(pipes[i]) < 0) {
 			perror("Plumbing problem.\n");
 			exit(1);
@@ -59,20 +83,30 @@ int main(int argc, char** argv) {
 			close(pipes[i][READ]);
 			// Read in.
 			char msg[MAX];
+			// Start loop for child.. always reading from its given pipe.
 			while (read(pipes[i-1][READ], &msg, MAX) > 0) {
 				sleep(1);
 				printf("Child %d with PID %d got the message: %s", i, getpid(), msg);
+				// If this is the destination we wanted, lets clear the message!
+				if (i == dest && dest_reached == 0) {
+					printf("Message reached destination! Clearing message from token.\n");
+					strcpy(msg, " \n");
+					dest_reached = 1;
+				}
+				// Write message to the next childs pipe.
 				write(pipes[i][WRITE], &msg, MAX);
 				printf("Child %d with PID %d sending message forward.\n", i, getpid());
 			}
+			// Clean up.
 			close(pipes[i-1][READ]);
 			close(pipes[i][WRITE]);
 			exit(0);
 		}
-		// parent code
-	}
+	} // Parent code.
+	// Init the signal handler.
+	sigaction(SIGINT, &sVal, NULL);
 	// Close pipes that the parent doesn't want.
-	for (int i = 1; i < k - 1; i++) {
+	for (int i = 1; i < k; i++) {
 		// Close write of pipe.
 		close(pipes[i][WRITE]);
 		// Close read of pipe.
@@ -81,24 +115,38 @@ int main(int argc, char** argv) {
 	// Close read of the first pipe.
 	close(pipes[0][READ]);
 	// Close write of the last pipe.
-	close(pipes[k - 1][WRITE]);
-
+	close(pipes[k][WRITE]);
+	// Set parents access to pipe 0 and pipe k.
 	c1_wr = pipes[0][WRITE];
-	ck_rd = pipes[k - 1][READ];
+	ck_rd = pipes[k][READ];
 
+	// Get message to pass along (we do this only once).
 	char payload[MAX];
 	printf("Please enter a message to pass around: ");
 	fgets(payload, 50, stdin);
 	printf("\nYou entered: %sI will now pass it around!\n", payload);
 
+	// Start loop for parent.. always reading from its given pipe.
 	while (1) {
 		write(c1_wr, payload, MAX);
 		read(ck_rd, payload, MAX);
 		printf("Parent with PID %d got message: %s", getpid(), payload);
+		// Clear message if there is one.
 		strcpy(payload, " \n");
 	}
-
+	// Clean up.
 	close(c1_wr);
 	close(ck_rd);
+
 	return 0;
+}
+
+/* Custom signal handler function. */
+void sigHandler(int sig, siginfo_t* si, void* context) {
+	if (SIGINT) {
+		sleep(2); // Wrap up processes then announce closing.
+		printf(" received an interrupt.\n");
+		printf("Closing token-ring.\n");
+		exit(0);
+	}
 }
